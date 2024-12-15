823f674e822cebed8f7f492c53cd52b37f808acf
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-12-15 13:48:16 +0100

    feat: Enhance websocket PR analysis with JWT authentication and error handling
    - Added JWT authentication to the websocket endpoint for pull request analysis.
    - Integrated dotenv to load environment variables.
    - Improved error handling for token decoding and websocket communication.
    - Updated websocket logic to handle client disconnections gracefully.
    - Added logging for better traceability of websocket events and errors.

--- app/routers/pr.py
+++ app/routers/pr.py
@@ -1,7 +1,12 @@
 import asyncio
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+import os
+from typing import Optional
+from dotenv import load_dotenv
+from fastapi import APIRouter, Header, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 import logging
+
+import jwt
 from utils import pr_analyzer
 
 logging.basicConfig(level=logging.INFO)
@@ -12,6 +17,7 @@ router = APIRouter(
     tags=["pull-request"],
     responses={404: {"description": "Not found"}},
 )
+
 html = """
 <!DOCTYPE html>
 <html>
@@ -67,29 +73,57 @@ async def get():
     logger.info("GET request received")
     return HTMLResponse(html)
 
+
 @router.get("/")
 async def read_root():
     return {"message": "Endpoint to get data from a pull request"}
 
+
 @router.websocket("/analysis")
-async def ws_pr_analysis(websocket: WebSocket, git_pr_url: str):
+async def ws_pr_analysis(websocket: WebSocket, supabase_token: Optional[str] = Header(None)):
     """
     Websocket endpoint for testing purposes.
     """
-    logger.info(f"Websocket connection established for PR: {git_pr_url}")
-    await websocket.accept()
+
+    load_dotenv()
+    JWT_SECRET = os.getenv("SUPABASE_JWT_SECRET",'')
+
+    if supabase_token != "supabase=token":
+        # Policy violaion
+        await websocket.close(code=1008)
     try:
-        await websocket.send_json({"status": "pending", "added_lines": [], "removed_lines": []})
-        await asyncio.sleep(0)
+        logger.error(f"Token : {supabase_token}" f"JWT_SECRET : {JWT_SECRET}")
+        payload = jwt.decode(
+            supabase_token, JWT_SECRET, algorithms=["HS256"], audience="authenticated"
+        )
+        logger.debug(f"Decoded payload : {payload}")
+        user_id = payload.get("sub")
+        # Check user_id correspond to the user who created the offer for selected repo
+    except Exception as error:
+        await websocket.send_error(message= f"An error has occured while decoding the token : {error}")
+        websocket.close(code=1008)
+
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_json()
+        git_pr_url = data.get("git_pr_url")
+        logger.info(f"Websocket connection established for PR: {git_pr_url}")
+
         try:
-            added_lines, removed_lines = pr_analyzer.get_data(git_pr_url)
-        except Exception as e:
-            await websocket.send_json({"status": "error", "error": str(e)})
+            await websocket.send_json({"status": "pending", "added_lines": [], "removed_lines": []})
+            await asyncio.sleep(0)
+            try:
+                added_lines, removed_lines = pr_analyzer.get_data(git_pr_url)
+            except Exception as e:
+                await websocket.send_json({"status": "error", "error": str(e)})
+                await asyncio.sleep(0)
+                await websocket.close()
+            #TODO: SAVE DATA IN DATABASE
+            # Instead of added_lines and removed_lines, we should check if error is handeled properly
+            await websocket.send_json({"status": "success", "added_lines": added_lines, "removed_lines": removed_lines})
             await asyncio.sleep(0)
             await websocket.close()
-        #TODO: SAVE DATA IN DATABASE
-        await websocket.send_json({"status": "success", "added_lines": added_lines, "removed_lines": removed_lines})
-        await asyncio.sleep(0)
-        await websocket.close()
-    except WebSocketDisconnect:
-        print("Client disconnected")
+        except WebSocketDisconnect:
+            # If the client disconnects, close the websocket connection
+            # We should continue the process in the background ?
+            print("Client disconnected")
